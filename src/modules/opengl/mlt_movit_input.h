a639485802be1bf6a55993113f051b6fab9d093b
Dan Dennedy <dan@dennedy.org>
2013-01-15 20:32:54 -0800

    Split mlt_movit_input into declaration and implementation.

--- src/modules/opengl/mlt_movit_input.h
+++ src/modules/opengl/mlt_movit_input.h
@@ -27,98 +27,26 @@
 class MltInput : public Input
 {
 public:
-	MltInput(unsigned width, unsigned height)
-		: m_width(width)
-		, m_height(height)
-		, output_linear_gamma(false)
-		, needs_mipmaps(false)
-		, input(0)
-		, isRGB(true)
-	{
-		register_int("output_linear_gamma", &output_linear_gamma);
-		register_int("needs_mipmaps", &needs_mipmaps);
-	}
-	~MltInput() {
-		delete input;
-	}
+	MltInput(unsigned width, unsigned height);
+	~MltInput();
 
 	// Effect overrides
 	std::string effect_type_id() const { return "MltInput"; }
-	std::string output_fragment_shader() {
-		assert(input);
-		return input->output_fragment_shader();
-	}
-	void set_gl_state(GLuint glsl_program_num, const std::string& prefix, unsigned *sampler_num) {
-		assert(input);
-		input->set_gl_state(glsl_program_num, prefix, sampler_num);
-	}
+	std::string output_fragment_shader();
+	void set_gl_state(GLuint glsl_program_num, const std::string& prefix, unsigned *sampler_num);
 
 	// Input ovverrides
-	void finalize() {
-		assert(input);
-		bool ok = input->set_int("output_linear_gamma", output_linear_gamma);
-		ok |= input->set_int("needs_mipmaps", needs_mipmaps);
-		assert(ok);
-		input->finalize();
-	}
-	bool can_output_linear_gamma() const {
-		assert(input);
-		return input->can_output_linear_gamma();
-	}
-	unsigned get_width() const {
-		return m_width;
-	}
-	unsigned get_height() const {
-		return m_height;
-	}
-	Colorspace get_color_space() const {
-		assert(input);
-		return input->get_color_space();
-	}
-	GammaCurve get_gamma_curve() const {
-		assert(input);
-		return input->get_gamma_curve();
-	}
+	void finalize();
+	bool can_output_linear_gamma() const;
+	unsigned get_width() const { return m_width; }
+	unsigned get_height() const { return m_height; }
+	Colorspace get_color_space() const;
+	GammaCurve get_gamma_curve() const;
 
 	// Custom methods
-	void useFlatInput(EffectChain* chain, MovitPixelFormat pix_fmt, unsigned width, unsigned height) {
-		if (!input) {
-			m_width = width;
-			m_height = height;
-			ImageFormat image_format;
-			image_format.color_space = COLORSPACE_sRGB;
-			image_format.gamma_curve = GAMMA_sRGB;
-			input = new FlatInput(image_format, pix_fmt, GL_UNSIGNED_BYTE, width, height);
-			chain->add_output(image_format);
-			chain->set_dither_bits(8);
-		}
-	}
-	void useYCbCrInput(EffectChain* chain, const ImageFormat& image_format, const YCbCrFormat& ycbcr_format, unsigned width, unsigned height) {
-		if (!input) {
-			m_width = width;
-			m_height = height;
-			input = new YCbCrInput(image_format, ycbcr_format, width, height);
-			ImageFormat output_format;
-			output_format.color_space = COLORSPACE_sRGB;
-			output_format.gamma_curve= GAMMA_sRGB;
-			chain->add_output(output_format);
-			chain->set_dither_bits(8);
-			isRGB = false;
-			m_ycbcr_format = ycbcr_format;
-		}
-	}
-	void set_pixel_data(const unsigned char* data) {
-		assert(input);
-		if (isRGB) {
-			FlatInput* flat = (FlatInput*) input;
-			flat->set_pixel_data(data);
-		} else {
-			YCbCrInput* ycbcr = (YCbCrInput*) input;
-			ycbcr->set_pixel_data(0, data);
-			ycbcr->set_pixel_data(1, &data[m_width * m_height]);
-			ycbcr->set_pixel_data(2, &data[m_width * m_height + (m_width / m_ycbcr_format.chroma_subsampling_x * m_height / m_ycbcr_format.chroma_subsampling_y)]);
-		}
-	}
+	void useFlatInput(EffectChain* chain, MovitPixelFormat pix_fmt, unsigned width, unsigned height);
+	void useYCbCrInput(EffectChain* chain, const ImageFormat& image_format, const YCbCrFormat& ycbcr_format, unsigned width, unsigned height);
+	void set_pixel_data(const unsigned char* data);
 
 private:
 	unsigned m_width, m_height;
